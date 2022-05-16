c5cc681b163f9d419aa10711b7eee9d846377aa6
Eder Diego de Sousa <me@diego.dev.br>
2022-05-16 13:52:05 -0300

    Custom ConstraintValidator

--- backend/src/main/java/br/dev/diego/dscatalog/controllers/dto/UserUpdateDto.java
+++ backend/src/main/java/br/dev/diego/dscatalog/controllers/dto/UserUpdateDto.java
@@ -1,5 +1,8 @@
 package br.dev.diego.dscatalog.controllers.dto;
 
+import br.dev.diego.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
 public class UserUpdateDto extends UserDto {
 
 }
