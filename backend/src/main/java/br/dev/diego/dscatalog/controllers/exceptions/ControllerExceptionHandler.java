ec19805888f8c01846ce0d1230213d06c8f1f36f
Eder Diego de Sousa <me@diego.dev.br>
2022-05-16 09:32:38 -0300

    Overriding MethodArgumentNotValidException

--- backend/src/main/java/br/dev/diego/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/br/dev/diego/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -2,14 +2,19 @@ package br.dev.diego.dscatalog.controllers.exceptions;
 
 import br.dev.diego.dscatalog.services.exceptions.DataNotFoundException;
 import br.dev.diego.dscatalog.services.exceptions.DatabaseException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -38,4 +43,19 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest req) {
+        ValidationExceptionDetails err = new ValidationExceptionDetails();
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
+        err.setTimeStamp(Instant.now());
+        err.setTitle("Method Argument Not Valid Exception, please check the documentation.");
+        err.setStatus(status.value());
+        err.setMessage(e.getMessage());
+        err.setPath(req.getContextPath());
+
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(fieldError -> err.addError(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return ResponseEntity.status(status).body(err);
+    }
 }
